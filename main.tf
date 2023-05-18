8bb0e98a62f51ff19ff8de2c7ff32b864092fda7
hyundeok-jo <chohdcg@lgupluspartners.co.kr>
2023-05-18 16:48:49 +0900

    추가

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
-
-l
 data "aws_organizations_organization" "main" {}
+
+data "aws_organizations_organizational_units" "parent" {
+  parent_id = data.aws_organizations_organization.main.roots[0].id
+}
+data "aws_organizations_organizational_units" "ou" {
+  parent_id = [for x in data.aws_organizations_organizational_units.parent.children : x.id if x.name == "Sandbox"][0]
+}
+
 
 data "aws_organizations_organization" "example" {}
 
@@ -8,14 +14,14 @@ output "account_ids" {
 #  value = data.aws_organizations_organization.example.accounts[*].name
   value = data.aws_organizations_organization.example.accounts[*].*
 
+}
+
 
 
 locals {
   account_map = { for account in data.aws_organizations_organization.example.accounts : account.name => account.id... }
   name_to_find = "hyun22"
   matching_id = local.account_map[local.name_to_find]
-
-  #matching_id = local.account_map[module.account_request_01.control_tower_parameters.AccountName]
 }
 
 output "matching_account_id" {
