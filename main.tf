aaa305131373a9efa71cbe559c15b30a41203f60
hyundeok-jo <chohdcg@lgupluspartners.co.kr>
2023-05-18 16:45:14 +0900

    output수정

--- main.tf
+++ main.tf
@@ -1,51 +1,12 @@
-data "aws_organizations_organization" "main" {}
 
-data "aws_organizations_organizational_units" "parent" {
-  parent_id = data.aws_organizations_organization.main.roots[0].id
-}
-data "aws_organizations_organizational_units" "ou" {
-  parent_id = [for x in data.aws_organizations_organizational_units.parent.children : x.id if x.name == "Sandbox"][0]
-}
+locals {
+  account_map = { for account in data.aws_organizations_organization.example.accounts : account.name => account.id... }
+  name_to_find = "hyun22"
+  matching_id = local.account_map[local.name_to_find]
 
-output "children" {
-  value = data.aws_organizations_organizational_units.ou.children
+  #matching_id = local.account_map[module.account_request_01.control_tower_parameters.AccountName]
 }
 
-
-## Sandbox2 OU
-data "aws_controltower_controls" "Sandbox2" {
-
-  target_identifier = [
-    for x in data.aws_organizations_organizational_units.ou.children :
-    x.arn if x.name == "Sandbox2"
-  ][0]
-
-}
-
-output "controls_security" {
-  value = data.aws_controltower_controls.Sandbox2.enabled_controls
-#  value = data.aws_controltower_controls.Sandbox2
-}
-
-variable "account_name" {
-  default = "hyun44"
-}
-
-resource "null_resource" "aws_cli_command" {
-  triggers = {
-    account_name = var.account_name
-  }
-
-  provisioner "local-exec" {
-    command = "aws organizations list-accounts --query 'Accounts[?Name==`${var.account_name}`].Id' --output text > account_id.txt"
-  }
-}
-
-data "local_file" "account_id" {
-#  filename = "${path.module}/account_id.txt"
-  depends_on = [null_resource.aws_cli_command]
-}
-
-output "account_id" {
-  value = data.local_file.account_id.content
+output "matching_account_id" {
+  value = local.matching_id
 }
