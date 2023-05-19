1b86b74e7c1aa87300798925550a2ac931d8662d
hyundeok-jo <chohdcg@lgupluspartners.co.kr>
2023-05-19 11:08:15 +0900

    account name 중복으로 허용 소스 수정

--- module/account.tf
+++ module/account.tf
@@ -5,10 +5,18 @@ variable "name_to_find" {
 
 data "aws_organizations_organization" "example" {}
 
+####account name 중복으로 허용 안함
+#locals {
+#  account_map = { for account in data.aws_organizations_organization.example.accounts : account.name => account.id }
+#  matching_id = local.account_map[var.name_to_find]
+#}
+
+####account name 중복으로 허용 소스 수정
 locals {
-  account_map = { for account in data.aws_organizations_organization.example.accounts : account.name => account.id }
+  account_map = { for account in data.aws_organizations_organization.example.accounts : account.name => account.id... }
   matching_id = local.account_map[var.name_to_find]
 }
+
 
 output "account_ids" {
   value = data.aws_organizations_organization.example.accounts[*].*
