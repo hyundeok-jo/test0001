147cd5e1204d8087f490a9ad0e2469877510916c
hyundeok-jo <chohdcg@lgupluspartners.co.kr>
2023-05-19 11:21:13 +0900

    기타 설정 제거

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-data "aws_organizations_organization" "main" {}
+#data "aws_organizations_organization" "main" {}
 
-data "aws_organizations_organizational_units" "parent" {
-  parent_id = data.aws_organizations_organization.main.roots[0].id
-}
+#data "aws_organizations_organizational_units" "parent" {
+#  parent_id = data.aws_organizations_organization.main.roots[0].id
+#}
 
-data "aws_organizations_organizational_units" "ou" {
-  parent_id = [for x in data.aws_organizations_organizational_units.parent.children : x.id if x.name == "Sandbox"][0]
-}
+#data "aws_organizations_organizational_units" "ou" {
+#  parent_id = [for x in data.aws_organizations_organizational_units.parent.children : x.id if x.name == "Sandbox"][0]
+#}
 
 module "account" {
   source = "./module"
